a78da80a2d193395a48ed51a1566a723cedb9dd6
muthu504 <102361374+muthu504@users.noreply.github.com>
2022-04-20 18:22:27 +0530

    commit
    Assignment 1

---
+++ FootballLeag/Win.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Win.aspx.cs" Inherits="FootballLeag.Win" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 320px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="Button2" runat="server" Height="28px" OnClick="Button2_Click" style="margin-top: 0px" Text="Draw" Width="94px" />
+        <asp:Button ID="Button1" runat="server" Text="Win"  OnClick="Button1_Click" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="JapanData" />
+        <asp:GridView ID="GridView1" runat="server" Height="232px" style="margin-left: 182px; margin-top: 10px; padding-top: 0px" Width="506px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ FootballLeag/Win.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FootballLeag
+{
+    public partial class Win : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+            GridView1.DataSource = data.FootBallLeagues;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            win();
+        }
+        private void win()
+        {
+            DataClasses1DataContext dataClasses1DataContext = new DataClasses1DataContext();
+            GridView1.DataSource = from win_status in dataClasses1DataContext.FootBallLeagues
+                                   where win_status.MatchStatus == "win"
+                                   select win_status;
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+        private void Draw()
+        {
+            DataClasses1DataContext dataClasses1DataContext1 = new DataClasses1DataContext();
+            GridView1.DataSource = from draw_status in dataClasses1DataContext1.FootBallLeagues
+                                   where draw_status.MatchStatus == "Draw"
+                                   select draw_status;
+            GridView1.DataBind();
+        }
+
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            japan();
+        }
+        private void japan()
+        {
+            DataClasses1DataContext dataClasses1DataContext = new DataClasses1DataContext();
+            GridView1.DataSource = from japan_Data in dataClasses1DataContext.FootBallLeagues
+                                   where japan_Data.TeamName1 == "japan" || japan_Data.TeamName2 == "japan"
+                                   select japan_Data;
+            GridView1.DataBind();
+        }
+    }
+}
